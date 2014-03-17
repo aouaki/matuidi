a9b941770c60673be349ac8090e6eda3a41e21f7
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-03-17 15:54:52 +0100

    Added basic form to index page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,22 @@
 angular.module('footbarGame', [])
 .controller('GameDetailCtrl', function ($scope, $http) {
-    $scope.game = 'PSG - OM';
+    $scope.game = 'PSGASSE';
+
+    $scope.getTrends = function() {
+        game = $scope.game;
+        $scope.trends
+        $scope.loading = true;
+        $http.get('/api/tweets/' + game).success(function(data){
+            $scope.loading = false;
+            $scope.trends = data.tweets.statuses;
+        })
+        .error(function(data){
+            $scope.loading=false;
+            $scope.error = 1
+        });
+    }
 });
+
+
+
+
