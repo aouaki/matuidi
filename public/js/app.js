91b4f2aa8bc6e42e4339622138fdae31233fdffe
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-03-17 10:42:59 +0100

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,4 @@
 angular.module('footbarGame', [])
-  .controller('GameDetailCtrl', function ($scope, $http) {
-    $scope.tweetReq = 0;
-    $scope.error = 0;
-    $scope.detected = false;
+.controller('GameDetailCtrl', function ($scope, $http) {
     $scope.game = 'PSG - OM';
-
-    $scope.getTrends = function() {
-        tweetReq = $scope.tweetReq;
-        $http.get('/api/tweets/' + tweetReq)
-      .success(function(data, status, headers, config) {
-          if (data.status) {
-              $scope.trends = data.trends[0].trends;
-          } else {
-              $scope.errorMsg = "Pas de tweets pour le match " + $scope.game + ". Réessayez plus tard.";
-              $scope.error = 1;
-          }
-      });
-    }
-
-  });
+});
