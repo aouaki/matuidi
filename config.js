391e964beda96fcac3f975b33aa4e8aced604d4e
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-03-17 11:10:00 +0100

    removed config file

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: '8KqOWXY1GLWDVeh6LgFPg',
-    consumer_secret: 'VgXFe7d34vPJZoalZ3yPmUs99TQkeJuwQqE2bGLJd4',
-    access_token: '557692776-bCtXnhFXyirRPjxK31OjEq5hR5iChFmZeG0j3YJk',
-    access_token_secret: 'bEFXq1jFUwFFjzfDX9hIETdHwfIoq31YXlf9Gk2hrixoo'
-};
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 /*
 Twitter API integration
 */
+var test = Number(process.env.PORT | 0);
+if (test==0){
+    var config = require('../config');
+}
+else {
+    var consumer_key= process.env.CONSUMER,
+    consumer_secret= process.env.CONSUMER_SECRET,
+    access_token= process.env.ACCESS,
+    access_token_secret= process.env.ACCESS_SECRET;
+};
+
 var Twit = require('twit'),
 config = require('../config'),
 T = new Twit(config);
