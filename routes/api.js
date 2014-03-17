73682a188768372eee645912d7f66848faaab77f
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-03-17 11:17:19 +0100

    wip

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,13 @@ if (test==0){
     var config = require('../config');
 }
 else {
-    var consumer_key= process.env.CONSUMER,
-    consumer_secret= process.env.CONSUMER_SECRET,
-    access_token= process.env.ACCESS,
-    access_token_secret= process.env.ACCESS_SECRET;
-};
+    module.exports = {
+        consumer_key: process.env.CONSUMER,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS,
+        access_token_secret: process.env.ACCESS_SECRET
+    };
+}
 
 var Twit = require('twit'),
 config = require('../config'),
