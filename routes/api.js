c5166adceaf09d310f769ddbb2a8d43b2ae52777
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-03-17 11:43:29 +0100

    wip heroku deployment

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,12 @@
 /*
-Twitter API integration
-*/
+   Twitter API integration
+   */
 var test = Number(process.env.PORT | 0);
-if (test==0){
+if (test==0) {
     var config = require('../config');
 }
 else {
-    var config = module.exports = {
+    var config = {
         consumer_key: process.env.CONSUMER,
         consumer_secret: process.env.CONSUMER_SECRET,
         access_token: process.env.ACCESS,
@@ -15,15 +15,15 @@ else {
 }
 
 var Twit = require('twit'),
-T = new Twit(config);
+    T = new Twit(config);
 
 exports.tweets = function(req, res) {
-  var tweetReq = req.params.tweetReq;
-  T.get('search/tweets', {q: '%23' + tweetReq + ' since:2014-02-14', count: 100}, function(err, data) {
-    if (typeof data === "undefined") {
-      res.json({status: false});
-    } else {
-      res.json({tweets: data, status: true});
-    }
-  });
+    var tweetReq = req.params.tweetReq;
+    T.get('search/tweets', {q: '%23' + tweetReq + ' since:2014-02-14', count: 100}, function(err, data) {
+        if (typeof data === "undefined") {
+            res.json({status: false});
+        } else {
+            res.json({tweets: data, status: true});
+        }
+    });
 };
