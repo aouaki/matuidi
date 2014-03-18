ccad7fbb154819e56933cd44707c83c24c303b2b
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-03-18 11:57:50 +0100

    Added retweet gestion. Corrected problem with ampersands.

--- public/js/app.js
+++ public/js/app.js
@@ -4,19 +4,47 @@ angular.module('footbarGame', [])
 
     $scope.getTrends = function() {
         game = $scope.game;
-        $scope.trends
         $scope.loading = true;
         $http.get('/api/tweets/' + game).success(function(data){
             $scope.loading = false;
-            $scope.trends = data.tweets.statuses;
+            trends = data.tweets.statuses;
+            for (trend in trends) {
+                temp = trends[trend].retweeted_status;
+                if (temp != undefined) {
+                    trends[trend] = temp;
+                }
+            }
+            $scope.trends = trends;
         })
         .error(function(data){
             $scope.loading=false;
             $scope.error = 1
         });
     }
+
+    $scope.noAmp= function (trend) {
+        console.log(trend.text);
+        text = trend.text;
+        text = text.replace('&amp;', '&');
+        trend.text = text;
+        return trend;
+    }
+})
+
+
+.filter('unique', function() {
+    return function(collection, keyname) {
+        var output = [], 
+        keys = [];
+
+        angular.forEach(collection, function(trend) {
+            var key = trend.id;
+            if(keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(trend);
+            }
+        });
+
+        return output;
+    };
 });
-
-
-
-
