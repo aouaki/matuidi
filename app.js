b8b78f7cf5859def422c8e3ac52ebea59688b55c
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-04-07 11:16:41 +0200

    Trying to remove CORS from herokuapp

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ server = require('http').createServer(app),
 routes = require('./routes'),
 api = require('./routes/api');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
 app.configure(function() {
+    app.use(allowCrossDomain);
     app.locals.pretty = true;
     app.use(express.bodyParser());
     app.use(express.methodOverride());
@@ -26,3 +41,5 @@ server.listen(PORT, function(){
 
 app.get('/', routes.index);
 app.get('/api/tweets/:tweetReq', api.tweets);
+
+
