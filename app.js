042990f82e3386292b09c572feb7ae6ffdc781ca
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-04-23 15:57:08 +0200

    Removed configure Express method

--- app.js
+++ app.js
@@ -23,17 +23,15 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
-app.configure(function() {
-    app.use(allowCrossDomain);
-    app.locals.pretty = true;
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/public'));
-    app.use('/components', express.static(__dirname + '/components'));
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use(app.router);
-    app.engine('html', require('ejs').renderFile);
-});
+app.use(allowCrossDomain);
+app.locals.pretty = true;
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+app.use('/components', express.static(__dirname + '/components'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use(app.router);
+app.engine('html', require('ejs').renderFile);
 
 server.listen(PORT, function(){
   console.log("Express server up and running.");
