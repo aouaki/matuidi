8d2755d9b3e2a5760e2348b38f18f10452f7d49b
Arthur Ouaki <arthur.ouaki@telecom-paristech.fr>
2014-04-23 16:24:08 +0200

    Mistake solved

--- routes/api.js
+++ routes/api.js
@@ -33,6 +33,7 @@ exports.tweets = function(req, res) {
 exports.someTweets = function(req, res) {
     var hashtag = req.params.hashtag;
     var tweetNb = req.params.tweetNb;
+    var id = req.params.id;
     T.get('search/tweets', {q: '%23' + hashtag + ' since:2013-08-01', geocode:['46.6', '1.88', '550km'], count: tweetNb, max_id:id}, function(err, data) {
         if (typeof data === "undefined") {
             res.json({status: false});
